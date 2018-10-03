808bba39ff671cdd215305aa973653295d82b640
Le Mouroux Alan <a.lemouroux.pro@gmail.com>
2018-10-03 22:25:44 +0200

    add setup.py and change project name (#2)

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+description =
+setup(
+    name='Shamania',
+    version='0.1.0',
+    description='Possess your shamans with artificial spirits. An AI fighting game.',
+    author='Alan Le Mouroux',
+    author_email='alan.le.mouroux@gmail.com',
+    packages=['server'],
+)
